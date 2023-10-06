9f8f0a490fb79376082dd542225b6c60d17b4430
RaghavSakhuja <raghav21274@iiitd.ac.in>
2023-10-07 01:05:22 +0530

    tcp

--- Ass2/assignment2/src/tcp_receiver.cc
+++ Ass2/assignment2/src/tcp_receiver.cc
@@ -7,20 +7,58 @@ using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
     const TCPHeader head = seg.header();
+    const Buffer &payload = seg.payload();
+    const WrappingInt32 seqno = head.seqno;
+    bool syn = head.syn;
+    
+    if (syn) {
+        _synReceived = true;
+        _isn = seqno;
+    }
+
+
+
+    if (_synReceived && head.fin) {
+        _finReceived = true;
+    }
+    if(!_synReceived) {
+        return;
+    }
+    if (_finReceived && _reassembler.empty()) {
+        _reassembler.stream_out().end_input();
+    }
+
+    uint64_t checkpoint = _reassembler.ack_index();
+    uint64_t abs_seqno = unwrap(head.seqno, _isn, checkpoint);
+    uint64_t stream_idx = abs_seqno - _synReceived;
+
+
+    if((_finReceived==false || (_finReceived && _reassembler.empty()==false)) && _synReceived==true){
+        _reassembler.push_substring(payload.copy(), stream_idx, head.fin);
+
+    }
+
+    
+
     // ...
 
     // --- Hint ------
         // convert the seqno into absolute seqno
-    uint64_t checkpoint = _reassembler.ack_index();
-    uint64_t abs_seqno = unwrap(head.seqno, _isn, checkpoint);
-    uint64_t stream_idx = abs_seqno - _synReceived;
     // --- Hint ------  
     // seg.payload.copy();
     // ... 
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_synReceived) {
+        return WrappingInt32(_reassembler.ack_index() + 1);
+    }
     return {};
 }
 
-size_t TCPReceiver::window_size() const { return 0; }
+size_t TCPReceiver::window_size() const { 
+    if (_synReceived) {
+        return _capacity - _reassembler.stream_out().buffer_size();
+    }
+    return 0;
+}
