71c4220e6d706266515fed88351e60b5b810b029
RaghavSakhuja <raghavsakhuja03@gmail.com>
2023-10-09 00:50:49 +0530

    all

--- Ass2/assignment2/src/stream_reassembler.cc
+++ Ass2/assignment2/src/stream_reassembler.cc
@@ -20,59 +20,83 @@ StreamReassembler::StreamReassembler(const size_t capa)
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof)
 {
+    // cout<<"---------------------\n";
     size_t end=index+data.size();
 
-    if(index>=acknowledged+capacity)
-        return;
+    // cout<<"data: "<<data<<" idx: "<<index<<" end:"<<end<<" ack:"<<acknowledged<<" unass:"<<unassembled<<" cap:"<<capacity<<endl;
 
-    packet p;
-    p.index=index;
-    p.length=data.size();
-    p.data=data;
+    if(index<acknowledged+capacity){
+        packet p;
+        p.index=index;
+        p.length=data.size();
+        p.data=data;
 
-    if(end<=acknowledged){
-        if(eof)
-            this->eof=true;
-        if(this->eof && unassembled==0)
-            _output.end_input();
-        return;
+        if(end<=acknowledged){
+            if(eof)
+                this->eof=true;
+            if(this->eof && unassembled==0)
+                _output.end_input();
+            return;
+        }
+        else if(index<acknowledged){
+            size_t offset=acknowledged-index;
+            p.index=acknowledged;
+            p.length=data.size()-offset;
+            p.data=data.substr(offset);
+        }
+        unassembled+=p.length;
+        // cout<<"initial buffer\n";
+        // for(auto i:buffer)
+        //     cout<<i.index<<" "<<i.length<<" "<<i.data<<endl;
+
+        // cout<<"unassembled: "<<unassembled<<endl;
+        for(size_t i=0;i<buffer.size();i++){
+            if(overlap(p,buffer[i])){
+                size_t merged=merging(p,buffer[i]);
+                // cout<<"merged: "<<merged<<endl;
+                unassembled-=merged;
+                buffer.erase(buffer.begin()+i);
+                i--;
+            }   
+        }
+        buffer.push_back(p);
     }
-    else if(index<acknowledged){
-        size_t offset=acknowledged-index;
-        p.index=acknowledged;
-        p.length=data.size()-offset;
-        p.data=data.substr(offset);
-    }
-    unassembled+=p.length;
 
-    for(size_t i=0;i<buffer.size();i++){
-        if(overlap(p,buffer[i])){
-            size_t merged=merging(p,buffer[i]);
-            unassembled-=merged;
-            buffer.erase(buffer.begin()+i);
-            i--;
-        }   
-    }
-
-    buffer.push_back(p);
+    // cout<<"unassembled: "<<unassembled<<endl;
     sort(buffer.begin(),buffer.end());
 
+
+    // cout<<"buffer\n";
     // for(auto i:buffer)
     //     cout<<i.index<<" "<<i.length<<" "<<i.data<<endl;
 
-    if(buffer[0].index==acknowledged){
-        packet p=buffer[0];
-        size_t bytes_written=_output.write(p.data);
-        unassembled-=bytes_written;
-        acknowledged+=bytes_written;
-        buffer.erase(buffer.begin());
-        packet p2;
-        p2.index=acknowledged;
-        p2.data=p.data.substr(bytes_written);
-        p2.length=p2.data.size();
-        buffer.insert(buffer.begin(),p2);
-        sort(buffer.begin(),buffer.end());
+    if(buffer.empty()){
+
     }
+    else{
+        if(buffer[0].index==acknowledged){
+            packet p=buffer[0];
+            size_t bytes_written=_output.write(p.data);
+            unassembled-=bytes_written;
+            acknowledged+=bytes_written;
+            buffer.erase(buffer.begin());
+            if(bytes_written!=p.length){
+                packet p2;
+                p2.index=acknowledged;
+                p2.data=p.data.substr(bytes_written,p.data.size()-bytes_written);
+                p2.length=p2.data.size();
+                buffer.insert(buffer.begin(),p2);
+                sort(buffer.begin(),buffer.end());
+            }
+        }
+
+        // cout<<"buffer2\n";
+        // for(auto i:buffer)
+        //     cout<<i.index<<" "<<i.length<<" "<<i.data<<endl;
+
+
+    }
+    cout<<"peek :"<<_output.peek_output(100)<<endl;
 
     // cout<<"unassembled: "<<unassembled<<endl;
     // cout<<"acknowledged: "<<acknowledged<<endl;
@@ -110,17 +134,29 @@ size_t StreamReassembler::ack_index() const {
 
 long StreamReassembler::merging(packet& p1, const packet& p2)
 {
-    packet smaller=p1.index<p2.index?p1:p2;
-    packet larger=p1.index<p2.index?p2:p1;
-    
-    if(smaller.index+smaller.length>=larger.index+larger.length){
-        p1=smaller;
-        return larger.length;
+    packet merged;
+
+
+
+    merged.index=min(p1.index,p2.index);
+    long length=0;
+    if(p1.index<=p2.index && p1.index+p1.length>=p2.index+p2.length){
+        merged.data=p1.data;
+        length=p2.length;
+    }
+    else if(p2.index<=p1.index && p2.index+p2.length>=p1.index+p1.length){
+        merged.data=p2.data.substr(0,p1.index-p2.index)+ p1.data+p2.data.substr(min(p1.index-p2.index+p1.length,p2.length));
+        length=p1.length;
+    }
+    else if(p2.index<p1.index){
+        merged.data=p2.data.substr(0,p1.index-p2.index)+p1.data;
+        length=p2.index+p2.length-p1.index;
     }
     else{
-        p1.index=smaller.index;
-        p1.data=smaller.data+larger.data.substr(smaller.index+smaller.length-larger.index);
-        p1.length=p1.data.size();
-        return smaller.index+smaller.length-larger.index;
+        merged.data=p1.data+p2.data.substr(min(p1.index+p1.length-p2.index,p2.length));
+        length=p1.index+p1.length-p2.index;
     }
+    merged.length=merged.data.size();
+    p1=merged;
+    return length;
 }
--- Ass2/assignment2/src/tcp_receiver.cc
+++ Ass2/assignment2/src/tcp_receiver.cc
@@ -10,10 +10,11 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     const Buffer &payload = seg.payload();
     const WrappingInt32 seqno = head.seqno;
     bool syn = head.syn;
-    cout<<"seqno: "<<seqno<<endl;
-    cout<<"syn: "<<syn<<endl;
-    cout<<"fin: "<<head.fin<<endl;
-    cout<<"payload size: "<<payload.size()<<endl;
+    // cout<<"seqno: "<<seqno<<endl;
+    // cout<<"syn: "<<syn<<endl;
+    // cout<<"fin: "<<head.fin<<endl;
+    // cout<<"payload size: "<<payload.size()<<endl;
+    // cout<<"ack: "<<ack<<endl;
     
     if (syn) {
         // if(_synReceived) {
@@ -23,32 +24,45 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         _isn = seqno;
         ack = seqno + 1;
     }
+    // cout<<"ack1: "<<ack<<endl;
 
     if(_synReceived==false){
         return;
     }
 
+    
     uint64_t checkpoint = _reassembler.ack_index();
-    uint64_t abs_seqno = unwrap(head.seqno, _isn, checkpoint);
+    uint64_t abs_seqno = unwrap(seqno, _isn, checkpoint);
     uint64_t stream_idx = abs_seqno - _synReceived;
-
-
+    // uint64_t stream_idx = abs_seqno;
+    // cout<<"abs_seqno: "<<abs_seqno<<endl;
+    // cout<<"stream_idx: "<<stream_idx<<endl;
+    // cout<<"checkpoint: "<<checkpoint<<endl;
+    
     if(_finReceived==false || (_finReceived && _reassembler.empty()==false)){
+        if(syn==true && payload.size()>0)
+            stream_idx=0;
         _reassembler.push_substring(payload.copy(), stream_idx, head.fin);
+    
     }
 
     uint64_t checkpoint2 = _reassembler.ack_index();
+    // cout<<"checkpoint2: "<<checkpoint2<<endl;
     ack = ack + checkpoint2 - checkpoint;
 
 
+    // cout<<"ack2: "<<ack<<endl;
+
+    cout<<_reassembler.empty()<<endl;
     if (_synReceived && head.fin) {
         _finReceived = true;
-        ack = ack + 1;
     }
     if (_finReceived && _reassembler.empty()) {
         _reassembler.stream_out().end_input();
+        ack = ack + 1;
     }
     
+    // cout<<"ack3: "<<ack<<endl;
 
     // ...
 
